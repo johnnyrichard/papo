df41e8041e33111553c2e36c8befca8ec3b05a09
Johnny Richard <johnny@johnnyrichard.com>
2022-04-07 04:18:03 +0200

    main.c: Sort includes

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <sys/socket.h>
-#include <string.h>
 #include <errno.h>
 #include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #define SERVER_PORT 6667
